9fb3b07c3293eaaa5caebe728711d4c036d0e16b
Clarkok <mail@clarkok.com>
2013-11-06 17:11:23 +0800

    Finish Events Class

--- js/events.js
+++ js/events.js
@@ -1,36 +1,50 @@
-Events = {
-    
-    add : function (_element, _event, _callback){
-        if (_element.addEventListener) {
-            _element.addEventListener(_event, _callback, false);
+Events = (function(){
+    function Events (){
+    }
+});
+Events.prototype._callback = function (_element, _event, _e) {
+    for (var i=0; i<_element._callbacks[_event].length; i++){
+        _element._callbacks[_event][i].call(_e);
+    }
+};
+Events.prototype.add = function (_element, _event, _callback){
+    _this = this;
+    _element._callbacks = _element._callbacks || {};
+    if (!_element._callbacks[_event]){
+        if (_element.addEventListener){
+            _element.addEventListener(_event, function(_e){
+                _this._callback(_element, _event, _e);
+            }, false);
+        }
+        else{
+            _element.attachEvent('on'+_event, function(){
+                _this._callback(_element, _event, window.event);
+            }, false);
+        }
+    }
+    _element._callbacks[_event] = _element._callbacks[_event] || [];
+    _element._callbacks[_event].push(_callback);
+};
+Events.prototype.del = function (_element, _event, _callback){
+    if (!_element._callbacks) return;
+    if (!_element._callbacks[_event]) return;
+    for (var i=0; i<_element._callbacks[_event].length; i++){
+        if (_element._callbacks[_event][i] == _callback){
+            return _element._callbacks[_event].splice(i, 1);
+        }
+    }
+};
+Events.prototype.dispatch = function (_element, _event) {
+    try {
+        if (_element.dispatchEvent){
+            var _e = document.createEvent('Event');
+            _e.initEvent(_event, true, true);
+            _element.dispatchEvent(_e);
         }
         else {
-            _element['__callback_'+_callback] = function(){
-                _callback.call(_element, evt);
-            };
-            _element.attachEvent('on'+_event, _element['__callback_'+_callback]);
+            _element.fireEvent('on'+_event);
         }
-    },
-    del : function (_element, _event, _callback){
-        if (_element.removeEventListener) {
-            _element.removeEventListener(_event, _callback, false);
-        }
-        else {
-            _element.detachEvent('on'+_event, _element['__callback_'+_callback]);
-        }
-    },
-    dispatch : function (_element, _event) {
-        try {
-            if (_element.dispatchEvent){
-                var _e = document.createEvent('Event');
-                _e.initEvent(_event, true, true);
-                _element.dispatchEvent(_e);
-            }
-            else {
-                _element.fireEvent('on'+_event);
-            }
-        } catch(e){
-            if (console) console.debug(e);
-        }
+    } catch(e){
+        if (console) console.debug(e);
     }
 };
